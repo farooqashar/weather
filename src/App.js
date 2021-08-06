d5a5f3b8c7605d0cc98395831b1d35adf7318308
Ashar Farooq <ashar744@gmail.com>
2021-08-05 22:22:22 -0400

    formatted code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import './App.css';
+import "./App.css";
 import Home from "./pages/Home.js";
-import {ApolloClient, InMemoryCache,ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 function App() {
-  
   const client = new ApolloClient({
     cache: new InMemoryCache(),
-    uri: "https://graphql-weather-api.herokuapp.com/"
+    uri: "https://graphql-weather-api.herokuapp.com/",
   });
-  
+
   return (
     <ApolloProvider client={client}>
-    <Home />
+      <Home />
     </ApolloProvider>
   );
 }
