2726e96cc07d269a1f942a2d4ad2e8f2e54bb0a1
Ashar Farooq <ashar744@gmail.com>
2021-08-05 21:13:10 -0400

    added basic home page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+        <h1>Find Weather By City</h1>
+        <input type="text" placeholder="Enter City Here"></input>
+        <button>Search</button>
+        </div>
+    )
+};
+
+export default Home;
